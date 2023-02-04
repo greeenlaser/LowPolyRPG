46f3588a1df71d7f6ee008dbc363c18651b52d02
greenlaser <55558448+greeenlaser@users.noreply.github.com>
2023-02-04 13:37:21 +0200

    added main menu
    -play - loads game scene
    -credits - shows game credits
    -return - returns back to main menu from credits
    -quit - quits the game

---
+++ Assets/Scripts/scr_UI/UI_MainMenu.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UI_MainMenu : MonoBehaviour
+{
+    [Header("UI")]
+    [SerializeField] private GameObject par_MMContent;
+    [SerializeField] private GameObject par_CreditsContent;
+    [SerializeField] private Button btn_Play;
+    [SerializeField] private Button btn_Credits;
+    [SerializeField] private Button btn_ReturnToMM;
+    [SerializeField] private Button btn_Quit;
+
+    private void Start()
+    {
+        btn_Play.onClick.AddListener(LoadGameScene);
+        btn_Credits.onClick.AddListener(OpenCredits);
+        btn_ReturnToMM.onClick.AddListener(OpenMM);
+        btn_Quit.onClick.AddListener(Quit);
+    }
+
+    //switch to game scene
+    public void LoadGameScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    //display all main menu content
+    public void OpenMM()
+    {
+        par_CreditsContent.SetActive(false);
+        par_MMContent.SetActive(true);
+    }
+    //display all credits menu content
+    public void OpenCredits()
+    {
+        par_MMContent.SetActive(false);
+        par_CreditsContent.SetActive(true);
+    }
+
+    //quit the game
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
